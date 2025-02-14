13b1d1bd95a9c94bc6a30a40c3d087c3b3fc4b08
Phong Phu <phongphunguyen7575@gmail.com>
2025-02-14 16:04:21 +0700

    TA6: Create CRUD for Teacher Table

---
+++ app/Models/QueryBuilders/TeacherQueryBuilder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Models\QueryBuilders;
+
+use App\Abstracts\AbstractQueryBuilder;
+use App\Models\SearchQueryBuilders\SearchQueryBuilder;
+use App\Models\Teacher;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\Concerns\SortsQuery;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class TeacherQueryBuilder extends AbstractQueryBuilder
+{
+    /**
+     * @return string
+     */
+    public static function baseQuery()
+    {
+        return Teacher::class;
+    }
+
+    /**
+     * @return SortsQuery|QueryBuilder
+     */
+    public static function initialQuery()
+    {
+        $modelKeyName = (new Teacher())->getKeyName();
+
+        return static::for(static::baseQuery())
+            ->allowedFields([
+                $modelKeyName,
+                'uuid',
+                'name',
+                'birthday',
+                'proper',
+                'created_at',
+                'updated_at'
+            ])
+            ->defaultSort('created_at')
+            ->allowedSorts([
+                $modelKeyName,
+                'uuid',
+                'name',
+                'birthday',
+                'proper',
+                'created_at',
+                'updated_at'
+            ])
+            ->allowedFilters([
+                $modelKeyName,
+                AllowedFilter::exact('exact__' . $modelKeyName, $modelKeyName),
+                'uuid',
+                AllowedFilter::exact('exact__uuid', 'uuid'),
+                'name',
+                AllowedFilter::exact('exact__name', 'name'),
+                'birthday',
+                AllowedFilter::exact('exact__birthday', 'birthday'),
+                'proper',
+                AllowedFilter::exact('exact__proper', 'proper'),
+                'created_at',
+                AllowedFilter::exact('exact__created_at', 'created_at'),
+                'updated_at',
+                AllowedFilter::exact('exact__updated_at', 'updated_at'),
+            ]);
+
+    }
+
+    /**
+     * @return string
+     */
+    public static function fillAble()
+    {
+        return Teacher::class;
+    }
+
+    /**
+     * @param $search
+     * @param $searchBy
+     * @return mixed
+     */
+    public static function searchQuery($search, $searchBy)
+    {
+        $initialQuery = static::initialQuery();
+        $baseQuery = static::fillAble();
+
+        return SearchQueryBuilder::search($baseQuery, $initialQuery, $search, $searchBy);
+    }
+}
--- database/migrations/2024_12_27_032320_create_teachers_table.php
+++ database/migrations/2024_12_27_032320_create_teachers_table.php
@@ -14,6 +14,8 @@ return new class extends Migration {
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->unsignedBigInteger('uuid')->autoIncrement();
+            $table->string('birthday');
+            $table->string('proper');
             $table->string('name');
             $table->timestamps();
         });
